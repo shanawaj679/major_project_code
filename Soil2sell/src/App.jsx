6baf63edf24f365235daddb3571ce06c4c3339d8
SHANAWAJ NADAF <147726542+shanawaj679@users.noreply.github.com>
2026-07-25 18:21:06 +0530

    added dashboard route

--- Soil2sell/src/App.jsx
+++ Soil2sell/src/App.jsx
@@ -1,13 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Register_user from "./register/register.jsx";
+import Dashboard from "./dashboard/dashboard.jsx";
 import Login from "./login/login.jsx";
 function App(){
   return <>
  <Routes>
 
       <Route path="/" element={<Register_user />} />
-
       <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />}/>
 
     </Routes>
   </>
