3063ab22e7a8388fb84bb4d2272003b3c9f5e423
SHANAWAJ NADAF <147726542+shanawaj679@users.noreply.github.com>
2026-07-28 22:01:59 +0530

    rootes are added into backend main folder

--- backend/rootbackend.js
+++ backend/rootbackend.js
@@ -5,15 +5,19 @@ import cookieParser from "cookie-parser"
 import login from "./login/login.js"
 import register from "./register/register.js"
 import db from "./database/database.js"
+import middleware_auth from "./middleware/middleware.js"
+import getuser_data from "./dashboard/getuser_data.js"
+import logout from "./login/logout.js"
 app.use(cookieParser())
 app.use(cors({
     origin:"http://localhost:5173",
     credentials:true
 }))
 app.use(express.json())
-console.log("Password:", process.env.mysql_password);
 app.use("/login",login)
 app.use("/register",register)
+app.use("/get_user",getuser_data)
+app.use("/log_out",logout)
 
 app.listen(3000,()=>{
     console.log("server is running on port no. 3000")
