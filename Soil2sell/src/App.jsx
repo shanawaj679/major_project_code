675f096cca75f2d56f4ac335c307eb5cfa04f7e3
SHANAWAJ NADAF <147726542+shanawaj679@users.noreply.github.com>
2026-08-01 20:17:56 +0530

    new routes are added in app.jsx

--- Soil2sell/src/App.jsx
+++ Soil2sell/src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import Register_user from "./register/register.jsx";
 import Dashboard from "./dashboard/dashboard.jsx";
 import Login from "./login/login.jsx";
+import Farmer_Details from "./farmer_details/farmer_details.jsx";
 function App(){
   return <>
  <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register_user />} />
       <Route path="/dashboard" element={<Dashboard />}/>
+      <Route path="/Farmer_Details" element={<Farmer_Details />}/>
 
     </Routes>
   </>
