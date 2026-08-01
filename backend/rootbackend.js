d573a06f74f7fd10efa4fc4420169fd8b2fa3baa
SHANAWAJ NADAF <147726542+shanawaj679@users.noreply.github.com>
2026-08-01 20:19:09 +0530

    new routes are added

--- backend/rootbackend.js
+++ backend/rootbackend.js
@@ -8,6 +8,8 @@ import db from "./database/database.js"
 import middleware_auth from "./middleware/middleware.js"
 import getuser_data from "./dashboard/getuser_data.js"
 import logout from "./login/logout.js"
+import farmer_details from "../backend/login/farmer_details/farmer_detail.js"
+import get_user_geo_data from "../backend/dashboard/get_user_geo_data.js"
 app.use(cookieParser())
 app.use(cors({
     origin:"http://localhost:5173",
@@ -18,6 +20,8 @@ app.use("/login",login)
 app.use("/register",register)
 app.use("/get_user",getuser_data)
 app.use("/log_out",logout)
+app.use("/farmer_details",farmer_details)
+app.use("/get_geo_data",get_user_geo_data)
 
 app.listen(3000,()=>{
     console.log("server is running on port no. 3000")
