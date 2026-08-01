de6f6746b1766000cf4a39d0873d9445ea919673
SHANAWAJ NADAF <147726542+shanawaj679@users.noreply.github.com>
2026-08-01 10:19:17 +0530

    dashboard updated

--- Soil2sell/src/dashboard/dashboard.jsx
+++ Soil2sell/src/dashboard/dashboard.jsx
@@ -4,7 +4,7 @@ import {motion} from "framer-motion"
 import axios from "axios"
 import { useState ,useEffect} from "react"
 function Dashboard(){
-    const [logout,setlogout]=useState("")
+const [logout,setlogout]=useState("")
 const [message,setmessage]=useState({})
 const [uservisible,setuservisible]=useState(true)
 const toggleinfo = ()=>{
@@ -40,12 +40,45 @@ catch(err){
 }
     
 }
-const fadeup={
-     initial:{opacity:0,scale:0.98},
-        whileInView:{opacity:1,scale:1},
-        transition:{duration:0.75},
-        viewport:{once:false,amount:0.3},     
+const fadeup = {
+  initial: { opacity: 0,y: 35, scale: 0.98, filter: "blur(6px)",},
+  whileInView: { opacity: 1,y: 0,scale: 1,filter: "blur(0px)",},
+  transition: {duration: 0.8,ease: [0.22, 1, 0.36, 1], },
+  viewport: {once: true,amount: 0.2,},
+};
+
+const hour = new Date().getHours();
+let greetings = "";
+if (hour >= 5 && hour < 12) {
+    greetings = "Good Morning";
+} else if (hour >= 12 && hour < 17) {
+    greetings = "Good Afternoon";
+} else if (hour >= 17 && hour < 21) {
+    greetings = "Good Evening";
+} else {
+    greetings = "Welcome Back";
 }
+const motivations = [
+ "🚜 Smart farming starts with smart decisions",
+ "🌾 Turning soil insights into better harvests",
+ "🌿 Grow smarter. Harvest better. Sell easier",
+ "📊 Everything you need for your farm, all in one dashboard",
+ "☀️ A new day, new opportunities for a better harvest",
+ "🌱 AI-powered farming for a sustainable tomorrow",
+ "🚀 Helping farmers make confident decisions every day",
+ "🌍 Better soil. Better crops. Better future",
+ "🤝 Connecting farmers directly to opportunities",
+ "🌱 Welcome back! Let’s cultivate success together",
+]
+let motivation = motivations[parseInt(Math.random() * motivations.length)]
+
+let date = new Date().toLocaleDateString("en-IN",{
+     weekday: "long",
+  day: "numeric",
+  month: "long",
+  year: "numeric",
+});
+
     return <>
     <header>
         <motion.div 
@@ -65,7 +98,21 @@ const fadeup={
     </header>
     <main>
         <div className="main_grid_1">
+            <motion.div {...fadeup}
+            transition={{...fadeup.transition, duration:0.8}} className="greet_container"> 
+                   <div className="greeting_section">
+                    <div>
+ <h2 className="greet">{greetings}</h2>
+ <h1 className="greet"><span>{message.name} 👋</span></h1>
+<p className="greet_subtitle">{motivation}</p>
+                    </div>
 
+                    <div>
+                          <h2>{date}</h2>
+                    </div>
+
+</div>
+ </motion.div>
         </div>
         <div  className="main_grid_2">
               {uservisible && (
